1e447ec39b9d9d1bc805dde20b63c2f340c15107
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2019-02-27 11:08:39 +0200

    Заглавен файл на точка в равнината

---
+++ geometry/point2d.h
@@ -0,0 +1,28 @@
+#ifndef __POINT2D_H
+#define __POINT2D_H
+
+class Point2D {
+  // Ниво 0
+  double x, y;
+
+public:
+  // Ниво 1: конструктори
+  Point2D();
+  Point2D(double x, double y);
+
+  // Ниво 1: селектори
+  double getX() const { return x; }
+  double getY() const { return y; }
+
+  // Ниво 1: мутатори
+  void setX(double x) { this->x = x; }
+  void setY(double y) { this->y = y; }
+
+  // Ниво 2: операции
+  void print() const;
+  double distanceToOrigin() const;
+  double distanceTo(Point2D p) const;
+  void translate(Point2D v);
+};
+
+#endif
